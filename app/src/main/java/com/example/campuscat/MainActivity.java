667b07843308811640515c64a1c047f266d38ff1
EUNU LEE <dmsdn0603@naver.com>
2025-05-11 22:54:15 +0900

    달력 초기 레이아웃 구성 작성자 : 이은우

--- app/src/main/java/com/example/campuscat/MainActivity.java
+++ app/src/main/java/com/example/campuscat/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.campuscat;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,7 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CalendarView calendarView = findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener((view, year, month, dayOfMonth) -> {
+            String date = year + "년 " + (month + 1) + "월 " + dayOfMonth + "일";
+            Toast.makeText(MainActivity.this, "선택한 날짜: " + date, Toast.LENGTH_SHORT).show();
+        });
     }
 }
+
 
 //캘린더 구현 메인 자바
