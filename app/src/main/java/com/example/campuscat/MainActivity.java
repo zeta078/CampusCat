4e2bb72096e753732c34f7bebb7b1188bd1a3f7b
EUNU LEE <dmsdn0603@naver.com>
2025-05-11 21:55:26 +0900

    시간표 레이아웃 초기 구성 작성자 : 이은우

--- app/src/main/java/com/example/campuscat/MainActivity.java
+++ app/src/main/java/com/example/campuscat/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.campuscat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonAdd = findViewById(R.id.btnAddTimetable);
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,
+                        "시간표 추가 및 등록 기능은 현재 개발 중입니다 ", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
