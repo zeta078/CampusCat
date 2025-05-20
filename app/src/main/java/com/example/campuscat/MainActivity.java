2917d68658354d3a5a50365bed3c82bd367b0e9e
danmuji0711 <61740604+danmuji0711@users.noreply.github.com>
2025-05-20 19:13:43 +0900

    플래너 양식 레이아웃 구현. 작성자 : 임석준

--- app/src/main/java/com/example/campuscat/MainActivity.java
+++ app/src/main/java/com/example/campuscat/MainActivity.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
