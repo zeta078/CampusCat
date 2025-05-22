8d94c2577834e28c55c30a2d416576740ea65ed3
danmuji0711 <61740604+danmuji0711@users.noreply.github.com>
2025-05-22 23:38:10 +0900

    Home 화면 전체적인 UI 디자인 수정. 수정자 : 임석준

--- app/src/main/java/com/example/campuscat/HomeActivity.java
+++ app/src/main/java/com/example/campuscat/HomeActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
 public class HomeActivity extends AppCompatActivity {
 
