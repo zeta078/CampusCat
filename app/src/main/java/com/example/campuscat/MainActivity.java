a425cbe38e24c1df1c3ebeb124d8e0909d0ab3e4
Joo6517 <dlwngks654@naver.com>
2025-05-27 22:33:18 +0900

    학식 기능 구현을 위한 레이아웃 구성 작성자:이주한

--- app/src/main/java/com/example/campuscat/MainActivity.java
+++ app/src/main/java/com/example/campuscat/MainActivity.java
@@ -1,16 +1,30 @@
 package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.MenuItem;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import androidx.appcompat.content.res.AppCompatResources;
 
 public class CafeteriaActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cafeteria);
+
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#D5ECFF")));
+            getSupportActionBar().setTitle("");
+
+            Drawable upArrow = AppCompatResources.getDrawable(this, androidx.appcompat.R.drawable.abc_ic_ab_back_material);
+            if (upArrow != null) {
+                upArrow.setColorFilter(Color.BLACK, PorterDuff.Mode.SRC_ATOP);
+                getSupportActionBar().setHomeAsUpIndicator(upArrow);
+            }
         }
     }
 
