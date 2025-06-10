ad4a373d9cd5a5f2a7af45c415bc318442cdeae6
EUNU LEE <dmsdn0603@naver.com>
2025-06-10 22:45:04 +0900

    dev브랜치와 home 브랜치 병합 작성자 : 이은우

--- app/src/main/java/com/example/campuscat/MoreFragment.java
+++ app/src/main/java/com/example/campuscat/MoreFragment.java
@@ -1,13 +1,10 @@
 package com.example.campuscat;
 
-import android.os.Bundle;
 import androidx.fragment.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 public class MoreFragment extends Fragment {
     public MoreFragment() {
         super(R.layout.fragment_more);
+
     }
 }
